26de980cc800f0700ec9186107c3fc33b359bc3a
simplenick <simplenick01@gmail.com>
2022-05-30 13:34:47 +0100

    Configured settings for image rendering

--- store/models.py
+++ store/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
 	name = models.CharField(max_length=200)
 	price = models.FloatField()
 	digital = models.BooleanField(default=False, null=True, blank=True)
-	# TODO: image
+	image = models.ImageField(null=True, blank=True)
 
 	def __str__(self):
 		return self.name
