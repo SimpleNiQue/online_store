6274a49a29cc2260de7e9ba9ce34dc83318a8189
simplenick <simplenick01@gmail.com>
2022-05-31 14:37:50 +0100

    Configured Checkout

--- store/models.py
+++ store/models.py
@@ -22,13 +22,13 @@ class Product(models.Model):
 		return self.name
 
 	@property
-	def imageUrl(self):
+	def imageURL(self):
 		try:
 			url = self.image.url
 		except:
 			url = ''
 		return url
-
+	
 
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -39,6 +39,22 @@ class Order(models.Model):
 	def __str__(self):
 		return str(self.id)
 
+	@property
+	def get_cart_items(self):
+		order_items = self.orderitem_set.all()
+		total = sum([item.quantity for item in order_items])
+
+		return total
+	
+
+	@property
+	def get_cart_total(self):
+		order_items = self.orderitem_set.all()
+		total = sum([item.get_total for item in order_items])
+
+		return total
+	
+
 
 class OrderItem(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
@@ -48,6 +64,12 @@ class OrderItem(models.Model):
 
 	def __str__(self):
 		return str(self.id)
+
+	@property
+	def get_total(self):
+		total = self.product.price * self.quantity
+		return total
+	
 
 
 class ShippingAddress(models.Model):
--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
+
 app_name = 'store'
 urlpatterns = [
   path('', views.store, name='store'),
